c8662fb235fafe83747b5c2fd32a6ffc777ced5c
Devanshu Gupta <devanshu.kissu.0000@gmail.com>
2023-05-24 23:41:42 +0530

    all

---
+++ idk.java
@@ -0,0 +1,11 @@
+package java;
+
+import java.util.Scanner;
+
+public class idk {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.next();
+        System.out.println("name");
+    }
+}
